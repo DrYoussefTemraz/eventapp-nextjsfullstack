372483aa87a8402f1335d1a4bf0ec96c86d130c8
DrYoussefTemraz <youseftahertemraz@gmail.com>
2026-02-03 14:14:16 +0200

    building the featured even map

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,14 @@ const page = () => {
       </h1>
       <p className='text-center mt-5'> Hackathons, Meetups, and Confrences all in one place</p>
       <ExploreBtn />
+      <div className='mt-20 space-y-7'>
+        <h3>Featured Events</h3>
+        <ul className='events'> 
+          {[1,2,3,4,5,6].map((event) => (
+          <ul key={event}>Event {event}</ul>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
